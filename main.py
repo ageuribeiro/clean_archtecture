059b89bf4620bf0110840ad41d0cc7f079c288cf
Ageu Lima Ribeiro <59054298+ageuribeiro@users.noreply.github.com>
2024-10-07 11:45:25 -0300

    Atualizando repositorio

---
+++ main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI, HTTPException
+from application.use_cases.consular_saldo import ConsultarSaldoUseCase
+from adapters.repository.contas_repository import ContaRepositoryMemoria
+from infrastructure.logging.logging_config import setup_logging
+import logging
+
+setup_logging()
+logger = logging.getLogger(__name__)
+
+app = FastAPI(title="Finanças Clean Architecure", version="1.0.0")
+
+contas_repo = ContaRepositoryMemoria()
+
+
+@app.on_event("startup")
+async def startup_event():
+    logger.info("Iniciando aplicação...")
+
+
+@app.get("/contas/{conta_id}/saldo", summary="Consulta o saldo de uma conta.")
+def consultar_saldo(conta_id: int):
+    logger.info(f"Recebida solicitação  de saldo para  a conta {conta_id}")
+    use_case = ConsultarSaldoUseCase(contas_repo)
+    try:
+        saldo = use_case.execute(conta_id)
+        logger.info(f"Saldo consultado para conta {conta_id} : {saldo}")
+        return {"conta_id": conta_id, "saldo": saldo}
+    except Exception as e:
+        logger.error(f" Erro ao consultar saldo para a conta {conta_id}: {e}", exc_info=True)
+        raise HTTPException(status_code=404, detail=str(e))
+    
+@app.on_event("shutdown")
+def shoutdown_event():
+    logger.info("Finalizando aplicação...")
